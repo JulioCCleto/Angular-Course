45992c94d43c074cfd92731768848a2fd871f9a1
JulioCCleto <44852279+JulioCCleto@users.noreply.github.com>
2020-03-28 12:27:29 -0300

    TemplateDrivenForms

---
+++ templateDrivenForms/src/app/form-group/form-group.component.ts
@@ -0,0 +1,33 @@
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
+
+@Component({
+  selector: 'app-form-group',
+  templateUrl: './form-group.component.html',
+  styleUrls: ['./form-group.component.css']
+})
+export class FormGroupComponent implements OnInit {
+
+  clientForm = new FormGroup({
+    firstName : new FormControl(''),
+    lastName : new FormControl(''),
+    name: new FormGroup({
+      firstName : new FormControl(''),
+      lastName : new FormControl(''),
+    })
+  });
+
+
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  submit() {
+    console.log(this.clientForm.value);
+    console.log(this.clientForm.value.firstName)
+    console.log(this.clientForm.value.name.firstName)
+  }
+
+  }
