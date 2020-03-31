893f525d02e4da3921d00291f0f5d51d2738dee1
JulioCCleto <44852279+JulioCCleto@users.noreply.github.com>
2020-03-31 19:59:04 -0300

    Crud PT 2

---
+++ crud/server/app.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const bodyparser = require('body-parser');
+const cors = require('cors');
+const mongoose = require('mongoose');
+const department_controller = require('./department_controller');
+const products_controller = require('./product_controller');
+const app = express();
+
+app.use(bodyparser.json());
+app.use(bodyparser.urlencoded({extended: true}));
+app.use(cors());
+
+mongoose.connect('mongodb://localhost:27017/http_app', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+
+app.use('/departments', department_controller);
+app.use('/products', products_controller);
+
+app.listen(3000);
